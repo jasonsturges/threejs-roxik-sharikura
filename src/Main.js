3d0c1c87a1bf5db7ed4e0a95576bb8c200eae37a
Jason Sturges <jason@jasonsturges.com>
2019-04-07 00:07:38 -0500

    Browser router basename

--- src/Main.js
+++ src/Main.js
@@ -3,7 +3,7 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Home from './containers/Home';
 
 const Main = () => (
-  <BrowserRouter>
+  <BrowserRouter basename="/threejs-roxik-sharikura">
     <Switch>
       <Route exact path='/' component={Home}></Route>
     </Switch>
