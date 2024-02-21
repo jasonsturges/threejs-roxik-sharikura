2b7fa6807a84bbb55bcc32ba12c613735ab15f4c
Jason Sturges <jason@jasonsturges.com>
2024-02-21 01:23:03 -0600

    Refactor interaction

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { MotionType } from "./MotionType.ts";
 import { changeMotion } from "./motion.ts";
 import { resize } from "./resize.ts";
-import "./keyboard.ts";
+import "./interaction.ts";
 import "./style.css";
 
 changeMotion(MotionType.CUBE);
